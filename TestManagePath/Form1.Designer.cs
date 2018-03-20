4206e29572f9e16d70ad771965b34e2911a88d9c
Marvin Zumbado <marvinzzz@gmail.com>
2018-03-20 14:56:34 -0600

    First load

---
+++ TestManagePath/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TestManagePath
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRequest = new System.Windows.Forms.TextBox();
+            this.btn_CallService = new System.Windows.Forms.Button();
+            this.btnCustomWay = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtRequest
+            // 
+            this.txtRequest.Location = new System.Drawing.Point(12, 98);
+            this.txtRequest.Multiline = true;
+            this.txtRequest.Name = "txtRequest";
+            this.txtRequest.Size = new System.Drawing.Size(481, 381);
+            this.txtRequest.TabIndex = 0;
+            this.txtRequest.Text = "<DGTicketRequest Version=\"1\" Identifier=\"b39ce249-febb-499e-9616-6b0c0854ed98\" Au" +
+    "thKey=\"MY_AUTH_KEY\" ApiKey=\"MY_KEY\" IncrementalBatchStartDate=\"03/20/2018 12:50:" +
+    "55 PM\"/>\r\n";
+            // 
+            // btn_CallService
+            // 
+            this.btn_CallService.Location = new System.Drawing.Point(12, 69);
+            this.btn_CallService.Name = "btn_CallService";
+            this.btn_CallService.Size = new System.Drawing.Size(103, 23);
+            this.btn_CallService.TabIndex = 1;
+            this.btn_CallService.Text = "Native";
+            this.btn_CallService.UseVisualStyleBackColor = true;
+            this.btn_CallService.Click += new System.EventHandler(this.btn_CallService_Click);
+            // 
+            // btnCustomWay
+            // 
+            this.btnCustomWay.Location = new System.Drawing.Point(189, 69);
+            this.btnCustomWay.Name = "btnCustomWay";
+            this.btnCustomWay.Size = new System.Drawing.Size(103, 23);
+            this.btnCustomWay.TabIndex = 3;
+            this.btnCustomWay.Text = "Custom";
+            this.btnCustomWay.UseVisualStyleBackColor = true;
+            this.btnCustomWay.Click += new System.EventHandler(this.btnCustomWay_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(517, 98);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(481, 381);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 528);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnCustomWay);
+            this.Controls.Add(this.btn_CallService);
+            this.Controls.Add(this.txtRequest);
+            this.Name = "Form1";
+            this.Text = "ManagePath WS tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRequest;
+        private System.Windows.Forms.Button btn_CallService;
+        private System.Windows.Forms.Button btnCustomWay;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TestManagePath/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml;
+using TestManagePath.ManagePathWebRef;
+
+namespace TestManagePath
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        readonly VendorTicketService _svc = new VendorTicketService();
+
+        private void btn_CallService_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnCustomWay.Enabled = false;
+                richTextBox1.Text = "";
+
+                var myXml = txtRequest.Text;
+                var xdoc = new XmlDocument();
+                xdoc.LoadXml(myXml);
+
+                var info = _svc.MPTicketService(xdoc);
+
+                richTextBox1.Text = info.ToString();
+                btnCustomWay.Enabled = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                richTextBox1.Text = ex.Message;
+                btnCustomWay.Enabled = true;
+            }
+        }
+
+        private static string SendRequestToMPAPI(string requestParams, string serviceURL)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            try
+            {
+
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;// | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+                var request = WebRequest.Create(serviceURL);
+                request.Headers.Add(@"SOAP:Action");
+                request.ContentType = "text/xml";
+                request.Method = "POST";
+                //custom envelope
+                var envelope = @"<?xml version=""1.0"" encoding=""utf-8"" ?><soap12:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap12=""http://www.w3.org/2003/05/soap-envelope"">  <soap12:Body>    <MPTicketService xmlns=""http://managepath8.com/"">      <Request>        REQUESTBODY      </Request>    </MPTicketService>  </soap12:Body></soap12:Envelope>";
+
+                var soapXml = envelope.Replace("REQUESTBODY", requestParams);
+
+                using (var writer = new StreamWriter(request.GetRequestStream()))
+                {
+                    writer.WriteLine(soapXml);
+                }
+
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (var readStream = new StreamReader(response.GetResponseStream()))
+                    {
+                        var resultXml = readStream.ReadToEnd();
+
+                        if (!String.IsNullOrEmpty(resultXml))
+                        {
+                            xmlDoc.LoadXml(resultXml);
+                        }
+                    }
+                    response.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                //GlobalLog.Log.Write("Exception received: Message: " + ex.Message + "InnerException Message: " + ex.InnerException.Message);
+            }
+            return xmlDoc.OuterXml;
+        }
+
+        private void btnCustomWay_Click(object sender, EventArgs e)
+        {
+       
+            btn_CallService.Enabled = false;
+            richTextBox1.Text = "";
+           var a =  SendRequestToMPAPI(txtRequest.Text,
+                "https://managepath8.com/Services/Implementations/DollarGeneral/VendorTicketService.asmx");
+
+
+            System.Xml.Linq.XDocument xDocument = System.Xml.Linq.XDocument.Parse(a);
+
+
+            richTextBox1.Text = xDocument.ToString();
+
+            btn_CallService.Enabled = true;
+        }
+
+
+    }
+}
